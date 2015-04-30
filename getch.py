f7b9c01843abda79d23bf1b3878fe8216c951c1b
Saleem Rashid <dev@saleemrashid.com>
2015-04-30 14:49:36 +0100

    getch: basic getch

---
+++ getch.py
@@ -0,0 +1,26 @@
+__all__ = ["getch"]
+
+def _load_getch():
+    try:
+        # POSIX
+        import termios
+    except ImportError:
+        # Windows
+        import msvcrt
+        return msvcrt.getch
+
+    # POSIX
+    import sys, tty
+    def _getch():
+        fd = sys.stdin.fileno()
+        restore = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, restore)
+        return ch
+
+    return _getch
+
+getch = _load_getch()
